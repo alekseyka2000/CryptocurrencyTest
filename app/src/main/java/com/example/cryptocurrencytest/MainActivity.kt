8b898fa5909745c5bdaea12d8db64a8e09deb2f6
Aleksey Yarakhovich <Alekseyka2000@gmail.com>
2020-12-09 22:51:57 +0300

    add StartFragment

--- app/src/main/java/com/example/cryptocurrencytest/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencytest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.cryptocurrencytest
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import com.example.cryptocurrencytest.databinding.ActivityMainBinding
 import com.example.cryptocurrencytest.di.cryptocurrencyServiceModule
 import com.example.cryptocurrencytest.model.cryptocurrencyapi.CryptocurrencyService
+import com.example.cryptocurrencytest.views.StartFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
@@ -14,26 +18,43 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.component.KoinApiExtension
 import org.koin.core.component.KoinComponent
 import org.koin.core.context.startKoin
+import java.util.*
+import kotlin.concurrent.schedule
 
 @KoinApiExtension
 class MainActivity : AppCompatActivity(), KoinComponent {
+
+    private lateinit var binding: ActivityMainBinding
+    private val startFragment by lazy {StartFragment()}
+
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .add(binding.container.id, startFragment)
+            .commit()
+
+        Timer().schedule(2000) {
+            supportFragmentManager.beginTransaction()
+                .remove(startFragment)
+                .commit()
+        }
 
         initKoin()
 
-        val cryptocurrencyService by inject<CryptocurrencyService>()
-
-        val view: TextView = findViewById(R.id.text)
-        cryptocurrencyService.getCryptocurrency()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { view.text = it.data[1].name },
-                { view.text = it.message }
-            )
+//        val cryptocurrencyService by inject<CryptocurrencyService>()
+//
+//        val view: TextView = findViewById(R.id.text)
+//        cryptocurrencyService.getCryptocurrency()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe(
+//                { view.text = it.data[1].name },
+//                { view.text = it.message }
+//            )
 
     }
 
