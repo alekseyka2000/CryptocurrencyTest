e9b1c8a1585faa5e72d70ffe22e54ef91a811eec
Aleksey Yarakhovich <Alekseyka2000@gmail.com>
2021-01-15 16:49:45 +0300

    fix Repository.kt

--- app/src/main/java/com/example/cryptocurrencytest/model/Repository.kt
+++ app/src/main/java/com/example/cryptocurrencytest/model/Repository.kt
@@ -5,11 +5,8 @@ import com.example.cryptocurrencytest.model.cryptocurrencyapi.CryptocurrencyServ
 import com.example.cryptocurrencytest.model.db.CryptocurrencyDB
 import com.example.cryptocurrencytest.model.entity.CryptocurrencyDataDB
 import com.example.cryptocurrencytest.model.entity.CryptocurrencyList
-import com.example.cryptocurrencytest.model.entity.Data
 import com.example.cryptocurrencytest.model.entity.PrepareCryptocurrencyData
-import io.reactivex.Observable
 import io.reactivex.Single
-import java.util.concurrent.TimeUnit
 
 class Repository(
     private val cryptocurrencyService: CryptocurrencyService,
@@ -27,33 +24,27 @@ class Repository(
     fun fetchSymbolURL() =
         cryptocurrencyService.getCryptocurrencyMetadata("BTC")
 
-    private fun dataMapping(p1: CryptocurrencyList): List<PrepareCryptocurrencyData> {
-
-//        Single.create<List<Data>> { emitter -> emitter.onSuccess(p1.data) }
-//            .delay
-        return p1.data.map { data ->
-            var symbol = "Empty"
-            cryptocurrencyService.getCryptocurrencyMetadata(data.symbol)
-                .subscribe({
-                    Log.d("TAG", "+")
-                    symbol = "it.data.bTC.logo" ?: "Null"
-                }, { e -> Log.e("TAG", e.message ?: "Error") })
-            db.cryptocurrencyDAO().insertData(
-                listOf(
-                    CryptocurrencyDataDB(
-                        imageReference = symbol,
-                        cryptocurrencyName = data.name,
-                        cryptocurrencyPriceUSD = data.quote.uSD.price,
-                        symbol = data.symbol,
-                        slug = data.slug,
-                        numMarketPairs = data.num_market_pairs,
-                        dateAdded = data.date_added
-                    )
+    private fun dataMapping(p1: CryptocurrencyList) = p1.data.map { data ->
+        var symbol = "Empty"
+        cryptocurrencyService.getCryptocurrencyMetadata(data.symbol)
+            .subscribe({
+                Log.d("TAG", "+")
+                symbol = "it.data.bTC.logo" ?: "Null"
+            }, { e -> Log.e("TAG", e.message ?: "Error") })
+        db.cryptocurrencyDAO().insertData(
+            listOf(
+                CryptocurrencyDataDB(
+                    imageReference = symbol,
+                    cryptocurrencyName = data.name,
+                    cryptocurrencyPriceUSD = data.quote.uSD.price,
+                    symbol = data.symbol,
+                    slug = data.slug,
+                    numMarketPairs = data.num_market_pairs,
+                    dateAdded = data.date_added
                 )
             )
+        )
 
-            PrepareCryptocurrencyData(symbol, data.name, data.quote.uSD.price + " $")
-        }
-        //return emptyList()
+        PrepareCryptocurrencyData(symbol, data.name, data.quote.uSD.price + " $")
     }
 }
